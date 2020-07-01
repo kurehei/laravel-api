8c37e6d5993632154ef6b9dca51e19852e3d71da
榑林晃平 <kureykou321@gmail.com>
2020-07-02 02:45:50 +0900

    controllerのApi化

---
+++ app/Http/Controllers/Api/PostsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function show() {
+
+    }
+
+    public function store() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function destroy() {
+
+    }
+    //
+}
