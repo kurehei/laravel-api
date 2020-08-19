1cda363135c2463efcebcca41461dc816e101d63
榑林晃平 <kureykou321@gmail.com>
2020-08-19 11:42:56 +0900

    fix .env

--- app/Http/Controllers/Api/V1_0/PostsController.php
+++ app/Http/Controllers/Api/V1_0/PostsController.php
@@ -31,6 +31,24 @@ class PostsController extends Controller
         $post->image = Storage::disk('s3')->url($path);
         $post->save();
         return $post;
+
+        $message = new Message;
+        // input()は、連想配列で返す
+        //input('カラム')でリクエストクラスに入った値を取得すr
+        $message->name = $request->input('name');
+        $message->content = $request->input('content');
+        $image =  $request->file("image");
+        // $imageを、myprefixというディレクトリに'public'という名前ど保存
+        $path = Storage::disk('s3')->putFile('myprefix', $image, 'public');
+        // 画像のurlを取得する
+        $message->image = Storage::disk('s3')->url($path);
+        // store()からファイルパスが返ってくる
+
+        //str_replaceメソッドで、filePathから、public以下を消去して、imageに格納
+        // hasは、リクエストに値が存在しているかどうかチェックするための関数
+        // $request->has('content')
+        $message->save();
+        return redirect('/');
     }
 
     public function update(Request $request, $id) {
